96a831f2e9d4f59287847f40f3e5bd4a8a86ab2e
shwijoyo <hadiwijoyo.suro@gmail.com>
2024-01-01 21:48:36 +0700

    fixed captureAll

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 (function($) {
-
-    "use strict";
-
-    var searchPopup = function() {
+    $(".navbar-toggler[data-bs-toggle='offcanvas']").on("click", function(){
+			$(".offcanvas").css({visibility: "visible"});
+			});
       // open search box
       $('#header-nav').on('click', '.search-button', function(e) {
         $('.search-popup').toggleClass('is-visible');
@@ -26,92 +25,4 @@
       $(document).keyup(function(b) {
           "27" === b.which && $(".search-popup").removeClass("is-visible")
       })
-    }
-
-    var initProductQty = function(){
-
-      $('.product-qty').each(function(){
-
-        var $el_product = $(this);
-        var quantity = 0;
-
-        $el_product.find('.quantity-right-plus').click(function(e){
-            e.preventDefault();
-            var quantity = parseInt($el_product.find('#quantity').val());
-            $el_product.find('#quantity').val(quantity + 1);
-        });
-
-        $el_product.find('.quantity-left-minus').click(function(e){
-            e.preventDefault();
-            var quantity = parseInt($el_product.find('#quantity').val());
-            if(quantity>0){
-              $el_product.find('#quantity').val(quantity - 1);
-            }
-        });
-
-      });
-
-    }
-
-    $(document).ready(function() {
-
-      searchPopup();
-      initProductQty();
-
-      var swiper = new Swiper(".main-swiper", {
-        speed: 500,
-        navigation: {
-          nextEl: ".swiper-arrow-prev",
-          prevEl: ".swiper-arrow-next",
-        },
-      });         
-
-      var swiper = new Swiper(".product-swiper", {
-        slidesPerView: 4,
-        spaceBetween: 10,
-        pagination: {
-          el: "#mobile-products .swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          0: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          980: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-          }
-        },
-      });      
-
-      var swiper = new Swiper(".product-watch-swiper", {
-        slidesPerView: 4,
-        spaceBetween: 10,
-        pagination: {
-          el: "#smart-watches .swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          0: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          980: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-          }
-        },
-      }); 
-
-      var swiper = new Swiper(".testimonial-swiper", {
-        loop: true,
-        navigation: {
-          nextEl: ".swiper-arrow-prev",
-          prevEl: ".swiper-arrow-next",
-        },
-      }); 
-
-    }); // End of a document ready
-
 })(jQuery);
